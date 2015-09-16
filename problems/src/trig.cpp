3ac9a8afd6a4329430e0c06e48550df054b6fbd8
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-15 23:21:14 -0500

    Trig#2 and Trig#3 solved

--- problems/src/trig.cpp
+++ problems/src/trig.cpp
@@ -8,3 +8,11 @@ double stairsLenght(double height, double angle){
     double distance = height / sin(rad(angle));
     return sqrt(pow(distance, 2) - pow(height, 2));
 }
+
+double circleArea(double radius){
+    return PI * pow(radius, 2);
+}
+
+double circlePerimeter(double radius){
+    return 2 * PI * radius;
+}
