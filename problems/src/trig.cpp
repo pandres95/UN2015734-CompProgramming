d56ccfcc806e6cf038088287afa5793001a8982c
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-15 23:19:12 -0500

    Fix problems

--- problems/src/trig.cpp
+++ problems/src/trig.cpp
@@ -1,10 +1,10 @@
 #include <project.h>
 
 double rad(double angle){
-    return deg * (PI / 180.0);
+    return angle * (PI / 180.0);
 }
 
 double stairsLenght(double height, double angle){
     double distance = height / sin(rad(angle));
-    return sqrt(pow(distance) - pow(height));
+    return sqrt(pow(distance, 2) - pow(height, 2));
 }
