a9642ca6d0a5e3c61932934be16d2a2fdf1c275c
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-16 02:09:33 -0500

    Biol problems solved

--- problems/src/biol.cpp
+++ problems/src/biol.cpp
@@ -1,1 +1,40 @@
 #include <project.h>
+
+using namespace std;
+
+void biol(){
+
+    // Problem #1
+	{
+		cout
+			<< endl << "Problema 1: Cuantos ºC son 25ºF"
+			<< endl << endl;
+
+		cout
+			<< endl << "25ºF = " << celsius(25) << "ºC."
+			<< endl << endl
+			<< endl;
+	}
+
+    // Problem #2
+	{
+		cout
+			<< endl << "Problema 2: Si una bacteria hace fusion binaria cada "
+                    << "20 minutos, cuanto tiempo tomara para tener 1 millon "
+                    << "de especies."
+			<< endl << endl;
+
+		cout
+			<< endl << "El tiempo necesario es de " << fuseTime() << " minutos."
+			<< endl << endl
+			<< endl;
+	}
+}
+
+double celsius(double fahrenheit){
+    return (fahrenheit - 32) * 5 / 9.0;
+}
+
+double fuseTime(){
+    return 20 * log(2, 1000000);
+}
