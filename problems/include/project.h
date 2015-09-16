289060a68ca452b2713b0cfedbd13551ba13ab88
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-15 23:11:48 -0500

    Added PI

--- problems/include/project.h
+++ problems/include/project.h
@@ -1,8 +1,15 @@
+#ifndef _PROJECT_H_
+#define _PROJECT_H_
+
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#define PI (atan(1) * 4)
+
 #include <trig.h>
 #include <phys.h>
 #include <chem.h>
 #include <biol.h>
 #include <geom.h>
+
+#endif
--- problems/src/trig.cpp
+++ problems/src/trig.cpp
@@ -1,5 +1,9 @@
 #include <project.h>
 
+double degToRad(double deg){
+    return deg * (PI / 180.0);
+}
+
 double stairsLenght(double height, double angle){
     return 0;
 }
