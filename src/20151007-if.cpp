0a188aec23206f4518e4de0f379a30413bb4e91b
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-10-07 14:41:47 -0500

    :Add sum/mult sample

--- src/20151007-if.cpp
+++ src/20151007-if.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+int y(int x){
+    int y;
+    if(x >= 0){
+        y = 0;
+        for(int i = 0; i < x; i++){
+            y += i;
+        }
+    } else {
+        y = 1;
+        for(int i = x; i < 0; i++){
+            y *= i;
+        }
+    }
+    return y;
+}
+
 int c(int a, int b) {
     if(a >= 0) {
         return a * b;
@@ -20,7 +36,7 @@ int main() {
     int a, b;
 
     while(cin >> a >> b){
-        cout << c(a, b) << endl;
+        cout << c(a, b) << ", " << y(a) << endl;
     }
 
     return 0;
